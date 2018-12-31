2ea677e016b180711646f327a4f624fc1a1e3700
Scott Lee <achacttn@gmail.com>
2019-01-01 00:42:51 +1000

    assets and components

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Compare.css';
 
 class Compare extends React.Component {
     render() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,23 +8,53 @@ class Home extends React.Component {
                 <div className="HomeBanner">
                     <h1 className="HomeBannerTitle">L&amp;S LED Australia NZ Pty Ltd</h1>
                 </div>
-                <div className="HomeSection1">
+                <div className="HomeGridContainer">
+                    <div className="HomeGrid">
+                        <div className="HomeSection HomeSection1">
+                            <div className="HomeSectionLeft HomeSection1"><h3>AC LED Technology</h3></div>
+                            <div className="HomeSectionRight HomeSection1">Image</div>
+                        </div>
+                        <div className="HomeSection HomeSection2">
+                            <div className="HomeSectionLeft HomeSection2"><h3>Smart Lights and IoT</h3></div>
+                            <div className="HomeSectionRight HomeSection2">Image</div>
+                        </div>
+                        <div className="HomeSection HomeSection3">
+                            <div className="HomeSectionLeft HomeSection3"><h3>Street Lights</h3></div>
+                            <div className="HomeSectionRight HomeSection3">Image</div>
+                        </div>
+                        <div className="HomeSection HomeSection4">
+                            <div className="HomeSectionLeft HomeSection4"><h3>Bay Lights</h3></div>
+                            <div className="HomeSectionRight HomeSection4">Image</div>
+                        </div>
+                        <div className="HomeSection HomeSection5">
+                            <div className="HomeSectionLeft HomeSection5"><h3>Tunnel Lights</h3></div>
+                            <div className="HomeSectionRight HomeSection5">Image</div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="HomeSection HomeSection1">
+                    <h3>L&S LEDs</h3>
+                    <p>L&amp;S is the world's leading manufacturer of AC LEDs.</p>
+                    <p>Established in 2015 in South Korea, our R&amp;D team has made breakthroughs in AC LED efficiency.</p>
+                    <p>We invite you to join us in revolutionizing outdoor lighting with our technology.</p>
+                </div>
+                <div className="HomeSection HomeSection2">
                     <h3>AC LED Technology</h3>
                     <p>Brief descripton</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vitae nihil ea labore quis dolor odio ducimus, modi sint in adipisci cupiditate tenetur dolores rem impedit quam voluptatibus ipsa consequuntur?</p>
                 </div>
-                <div className="HomeSection2">
+                <div className="HomeSection HomeSection3">
                     <h3>Street Lights</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eaque deleniti cumque praesentium consequuntur nemo quisquam officiis doloremque dignissimos temporibus blanditiis minima repellendus, saepe obcaecati, maxime numquam sapiente culpa voluptatem.</p>
                 </div>
-                <div className="HomeSection3">
+                <div className="HomeSection HomeSection4">
                     <h3>Bay Lights</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, unde! Rerum reiciendis provident, itaque fugiat nam est iusto perferendis adipisci, voluptas repudiandae at dolor ullam delectus quaerat voluptate nobis corrupti.</p>
                 </div>
-                <div className="HomeSection4">
+                <div className="HomeSection HomeSection5">
                     <h3>Tunnel Lights</h3>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet alias possimus, ab suscipit aperiam saepe atque earum at ad? Ipsam asperiores doloremque quibusdam tempora fugiat animi optio, tenetur recusandae aperiam?</p>
-                </div>
+                </div> */}
             </div>
         )
     }
