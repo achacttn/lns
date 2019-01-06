ff862a2014b534e1502f36138bc962d1702d92de
Scott Lee <achacttn@gmail.com>
2019-01-06 16:58:16 +1100

    homepage hover styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,71 +20,47 @@ class Home extends React.Component {
                 </div>
                 <div className="HomeGridContainer">
                     <div className="HomeGrid">
-                        <div className="HomeSectionLeft HomeSection1 HomeSection">
-                            <div className="HomeSectionContent">
+                        <div className="HomeGridRow HomeGridRow1">
+                            <div className="HomeSectionLeft HomeSection">
                                 <h3>AC LED Technology</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum atque excepturi illo qui dolorum, molestiae saepe cumque quaerat. Consectetur alias molestiae repudiandae harum accusamus numquam optio doloremque adipisci recusandae!</p>
                             </div>
+                            <div className="HomeSectionRight HomeSection"></div>
                         </div>
-                        <div className="HomeSectionRight HomeSection1 HomeSection">
-                            <div className="HomeSectionContent">
-                                <div className="HomeSectionImage1"></div>
-                            </div>
-                        </div>
-                        <div className="HomeSectionLeft HomeSection2 HomeSection">
-                            <div className="HomeSectionContent">
+                        <div className="HomeGridRow HomeGridRow2">
+                            <div className="HomeSectionLeft HomeSection">
                                 <h3>Smart Lights and IoT</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum atque excepturi illo qui dolorum, molestiae saepe cumque quaerat. Consectetur alias molestiae repudiandae harum accusamus numquam optio doloremque adipisci recusandae!</p>
                             </div>
+                            <div className="HomeSectionRight HomeSection"></div>
                         </div>
-                        <div className="HomeSectionRight HomeSection2 HomeSection">
-                            <div className="HomeSectionContent">
-                                <div className="HomeSectionImage2"></div>
-                            </div>
-                        </div>
-                        <div className="HomeSectionLeft HomeSection3 HomeSection">
-                            <div className="HomeSectionContent">
+                        <div className="HomeGridRow HomeGridRow3">
+                            <div className="HomeSectionLeft HomeSection">
                                 <h3>Street Lights</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum atque excepturi illo qui dolorum, molestiae saepe cumque quaerat. Consectetur alias molestiae repudiandae harum accusamus numquam optio doloremque adipisci recusandae!</p>
                             </div>
+                            <div className="HomeSectionRight HomeSection"></div>
                         </div>
-                        <div className="HomeSectionRight HomeSection3 HomeSection">
-                            <div className="HomeSectionContent">
-                                <div className="HomeSectionImage3"></div>
-                            </div>
-                        </div>
-                        <div className="HomeSectionLeft HomeSection4 HomeSection">
-                            <div className="HomeSectionContent">
+                        <div className="HomeGridRow HomeGridRow4">
+                            <div className="HomeSectionLeft HomeSection">
                                 <h3>Bay Lights</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum atque excepturi illo qui dolorum, molestiae saepe cumque quaerat. Consectetur alias molestiae repudiandae harum accusamus numquam optio doloremque adipisci recusandae!</p>
                             </div>
+                            <div className="HomeSectionRight HomeSection"></div>
                         </div>
-                        <div className="HomeSectionRight HomeSection4 HomeSection">
-                            <div className="HomeSectionContent">
-                                <div className="HomeSectionImage4"></div>
-                            </div>
-                        </div>
-                        <div className="HomeSectionLeft HomeSection5 HomeSection">
-                            <div className="HomeSectionContent">
+                        <div className="HomeGridRow HomeGridRow5">
+                            <div className="HomeSectionLeft HomeSection">
                                 <h3>Tunnel Lights</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum atque excepturi illo qui dolorum, molestiae saepe cumque quaerat. Consectetur alias molestiae repudiandae harum accusamus numquam optio doloremque adipisci recusandae!</p>
                             </div>
+                            <div className="HomeSectionRight HomeSection"></div>
                         </div>
-                        <div className="HomeSectionRight HomeSection5 HomeSection">
-                            <div className="HomeSectionContent">
-                                <div className="HomeSectionImage5"></div>
-                            </div>
-                        </div>
-                        <div className="HomeSectionLeft HomeSection6 HomeSection">
-                            <div className="HomeSectionContent">
+                        <div className="HomeGridRow HomeGridRow6">
+                            <div className="HomeSectionLeft HomeSection">
                                 <h3>Stadium Lights</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut rerum, voluptatum doloremque ab ipsa est optio odit hic debitis itaque dolores illum deleniti voluptate quam animi possimus placeat libero doloribus.</p>
                             </div>
-                        </div>
-                        <div className="HomeSectionRight HomeSection6 HomeSection">
-                            <div className="HomeSectionContent">
-                                <div className="HomeSectionImage6"></div>
-                            </div>
+                            <div className="HomeSectionRight HomeSection"></div>
                         </div>
                     </div>
                 </div>
